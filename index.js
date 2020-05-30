d087159b0b90479b403dc5c2dddcd95d15a4972a
Antony Sanders <antony.devon92@gmail.com>
2020-05-30 06:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function renderBooks(books) {
     const main = document.querySelector('main')
     books.forEach(book => {
         const h2 = document.createElement('h2')
-        h2.innerHTML = book.name + ", " + book.authors + ", " + book.numberOfPages
+        h2.innerHTML = book.name + " was wriiten by " + book.authors + " and has " + book.numberOfPages + " pages!"
         main.appendChild(h2)
     })
 }
